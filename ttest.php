277eb9ec1e948b9c1141b5c38bf3cce7af4bcece
fixian <fixian@gmail.com>
2014-05-30 23:45:03 +0900

    test addt

---
+++ ttest.php
@@ -0,0 +1,33 @@
+리스트
+$wr_10 = $list[$i]['wr_10'];
+
+뷰
+$wr_10 = $view['wr_10'];
+
+
+if($wr_10){
+    $tmp = explode("|",$wr_10)
+    //$tmp = list($a,$b,$c) = explode("|",$wr_10)
+    
+    switch ($tmp) {
+        case 'a' : $str = '문자1' ; break; 
+        case 'b' : $str = '문자2' ; break;  
+        case 'c' : $str = '문자3' ; break;  
+        default :  $str = '' ; break;
+    }
+}
+
+echo $str;
+
+
+function debug ($data) { 
+    echo "<script>\r\n//<![CDATA[\r\nif(!console){var console={log:function(){}}}"; 
+    $output    =    explode("\n", print_r($data, true)); 
+    foreach ($output as $line) { 
+        if (trim($line)) { 
+            $line    =    addslashes($line); 
+            echo "console.log(\"{$line}\");"; 
+        } 
+    } 
+    echo "\r\n//]]>\r\n</script>"; 
+} 
